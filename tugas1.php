93db41fd48d37f02c107ee8a966f95c844d115ca
Andika Andriana <andriana.mia1@gmail.com>
2019-12-27 17:59:36 +0700

    Tugas 1 PHP
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas1.php
@@ -0,0 +1,24 @@
+<?php
+  echo("<h1>Daftar Menu</h1>");
+
+  $ayamGoreng = 13000;
+  $ayamBakar = 15000;
+  $ayamSayur = 13000;
+  $tempeGoreng = 1000;
+  $tahuGoreng = 1000;
+  $soto = 3000;
+  $nasiPutih = 5000;
+  $esTeh = 4000;
+  $esTebu = 5000;
+
+  echo("
+    <li>Ayam Goreng = Rp. $ayamGoreng</li>
+    <li>Ayam Bakar = Rp. $ayamBakar</li>
+    <li>Ayam Sayur = Rp. $ayamSayur</li>
+    <li>Temoe Goreng = Rp. $tempeGoreng</li>
+    <li>Soto = Rp. $soto</li>
+    <li>Nasi Putih = Rp. $nasiPutih</li>
+    <li>Es Teh = Rp. $esTeh</li>
+    <li>Es Tebu = Rp. $esTebu</li>
+  ");
+ ?>
---
+++ tugas1_2.php
@@ -0,0 +1,48 @@
+<?php
+  echo("<h1>Daftar Menu</h1>");
+
+  $cireng = "Rp 1000";
+  $esDuren = "Rp 15000";
+  $esMilo = "Rp 20000";
+  $esTeh = "Rp 5000";
+  $piscok = "Rp 1000";
+
+  echo("
+  <table border =1>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama Menu</th>
+            <th>Harga</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Cireng</td>
+            <td>$cireng</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Es Duren</td>
+            <td>$esDuren</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Es Milo</td>
+            <td>$esMilo</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Es Teh</td>
+            <td>$esTeh</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Piscok</td>
+            <td>$piscok</td>
+        </tr>
+    </tbody>
+  </table>
+  ");
+ ?>
